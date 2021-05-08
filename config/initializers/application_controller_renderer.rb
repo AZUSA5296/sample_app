34eb0846421a79dd01fbf881104947567d1f8912
EC2 Default User <ec2-user@ip-172-31-36-239.ec2.internal>
2021-05-08 18:20:47 +0000

    [update]編集機能、画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7499a38209a21f1e99e2bf9f2d1e586452b4835a1944b9a704422ae94c9360ea67406f9c75caecd42af524d61bea9f621f4aa301beae22914212713859a09ef0'
